b0ae59ea1de5fdbbb70907729237e1c65b8edd5b
zakiansyah4848 <ansyahzaki69@gmail.com>
2023-12-08 23:24:59 +0700

    Add Scan History with Dummy Data

--- app/src/main/java/com/dicoding/scancare/ui/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/scancare/ui/main/MainActivity.kt
@@ -7,13 +7,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.scancare.HistoryItem
 import com.dicoding.scancare.databinding.ActivityMainBinding
+import com.dicoding.scancare.ui.HistoryAdapter
+import com.dicoding.scancare.ui.detail.DetailActivity
+import com.dicoding.scancare.ui.result.ResultActivity
 import com.dicoding.scancare.ui.scan.ScanImageActivity
 import com.dicoding.scancare.utils.getImageUri
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var currentImageUri: Uri? = null
+
+    private val historyList = mutableListOf<HistoryItem>()
+    private lateinit var historyAdapter: HistoryAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +36,21 @@ class MainActivity : AppCompatActivity() {
             startCamera()
         }
 
+        // mengatur adapter untuk history
+        historyAdapter = HistoryAdapter(historyList) { historyItem ->
+            navigateToResultActivity(historyItem)
+        }
+        binding.recyclerViewHistory.layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewHistory.adapter = historyAdapter
+
+        // data dummy history
+        historyList.add(HistoryItem("Skintific Pure Centella Acne Calming Toner", "Aplha Hydroxy Acids, Retinoids", "2023-01-01", "12:00 PM"))
+
+        // memberitahu adapter data sudah dibah
+        historyAdapter.notifyDataSetChanged()
+
+
+
     }
 
     private fun startCamera() {
@@ -38,6 +62,17 @@ class MainActivity : AppCompatActivity() {
     private fun startGallery() {
         launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
+
+    private fun navigateToResultActivity(historyItem: HistoryItem) {
+        val intent = Intent(this, ResultActivity::class.java).apply {
+            putExtra("HISTORY_NAMA_PRODUK", historyItem.namaProduk)
+            putExtra("HISTORY_INGREDIENTS", historyItem.ingredients)
+            putExtra("HISTORY_DATE", historyItem.date)
+            putExtra("HISTORY_TIME", historyItem.time)
+        }
+        startActivity(intent)
+    }
+
 
     private val launcherGallery = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()
