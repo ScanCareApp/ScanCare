264a8ffb83b2c4a6cbc61e641ef14000732eb174
zakiansyah4848 <ansyahzaki69@gmail.com>
2023-12-10 02:42:56 +0700

    Add Login Register With Firebase email and password (without name)

--- app/src/main/java/com/dicoding/scancare/ui/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/scancare/ui/login/LoginActivity.kt
@@ -1,14 +1,82 @@
 package com.dicoding.scancare.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.dicoding.scancare.R
+import com.dicoding.scancare.databinding.ActivityLoginBinding
+import com.dicoding.scancare.ui.main.MainActivity
+import com.dicoding.scancare.ui.register.RegisterActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        // tes commit and push ke organisasi
+    lateinit var binding: ActivityLoginBinding
+    lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+
+        binding.tvRegisterNow.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.loginEmailInputEditText.text.toString()
+            val password = binding.loginPasswordInputEditText.text.toString()
+
+            //validasi email
+            if (email.isEmpty()){
+                binding.loginEmailInputEditText.error = "email harus diisi"
+                binding.loginEmailInputEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi email tidak sesuai
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.loginEmailInputEditText.error = "email tidak valid"
+                binding.loginEmailInputEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi password
+            if (password.isEmpty()){
+                binding.loginPasswordInputEditText.error = "password harus diisi"
+                binding.loginPasswordInputEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi panjang password
+
+            if (password.length < 8){
+                binding.loginPasswordInputEditText.error = "password minimal mengandung 8 karakter"
+                binding.loginPasswordInputEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            LoginFirebase(email,password)
+        }
+    }
+
+    private fun LoginFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this,"Login Berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/dicoding/scancare/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/scancare/ui/register/RegisterActivity.kt
@@ -1,12 +1,78 @@
 package com.dicoding.scancare.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.scancare.R
+import android.util.Patterns
+import android.widget.Toast
+import com.dicoding.scancare.databinding.ActivityRegisterBinding
+import com.dicoding.scancare.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityRegisterBinding
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.tvLoginNow.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val email = binding.emailEditTextInput.text.toString()
+            val password = binding.passwordEditTextInput.text.toString()
+
+            //validasi email
+            if (email.isEmpty()){
+                binding.emailEditTextInput.error = "email harus diisi"
+                binding.emailEditTextInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi email tidak sesuai
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.emailEditTextInput.error = "email tidak valid"
+                binding.emailEditTextInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi password
+            if (password.isEmpty()){
+                binding.passwordEditTextInput.error = "password harus diisi"
+                binding.passwordEditTextInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi panjang password
+
+            if (password.length < 8){
+                binding.passwordEditTextInput.error = "password minimal mengandung 8 karakter"
+                binding.passwordEditTextInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            RegisterFirebase(name,email,password)
+        }
+    }
+
+    private fun RegisterFirebase(name: String, email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this,"Register Berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,LoginActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
